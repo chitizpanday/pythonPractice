7554240cdea0bb7f61ea661258b889c600ccf606
chitiz panday <pandchit@isu.edu>
2023-07-17 15:03:22 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#name_Input = input("whats your name")
+#print("your name is " + name_Input)
+#print(type(nameInput))
+#================================================================
+#first_name = "chitiz "
+#last_name = "panday"
+#full_name = first_name + last_name
+#print("my full name is: " + full_name)
+#==================================================================================
+#age = 6
+#print("My sons age is " + str(age)) #here we performed type casting by changing integer data type to string data type
+#=================================================================================
+#name, age, height = "chitiz panday", 41, 5.5
+#print(name,age,height)
+#====================================
+name="chitiz"
+print(len(name))
+print(name.find('z'))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isalpha())
+print(name.isdigit())
+print(name.count('i'))
+print(name.replace('c', 'a'))
+print(name*3)
+#=========================================
+
